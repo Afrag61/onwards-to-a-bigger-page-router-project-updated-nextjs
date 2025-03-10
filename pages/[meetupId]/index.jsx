5a4e33dc85ecbaf6f0e5752bc1fc6b0764494884
Ahmed Frag <a.frag166@gmail.com>
2025-03-10 17:26:26 +0200

    Adding Custom Components & CSS Modules

---
+++ pages/[meetupId]/index.jsx
@@ -0,0 +1,18 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <>
+      <MeetupDetail
+        title={"A First Meetup"}
+        image={
+          "https://alumni.ubc.ca/wp/wp-content/uploads/MyTownMeetups-1270x460-1.jpg"
+        }
+        address={'Some Address 5, 12345 Some City'}
+        description={'this is A First Meetup'}
+      />
+    </>
+  );
+};
+
+export default MeetupDetails;
