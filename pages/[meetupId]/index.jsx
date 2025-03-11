26b666f49399c0275a07a6e947f82d38caff9987
Ahmed Frag <a.frag166@gmail.com>
2025-03-11 20:42:59 +0200

    Working with Params for SSG Data Fetching

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,4 +1,5 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { print } from "@/Print";
 
 const MeetupDetails = () => {
   return (
@@ -13,6 +14,26 @@ const MeetupDetails = () => {
       />
     </>
   );
+};
+
+export const getStaticProps = async (context) => {
+  // fetch data from api
+  const meetupId = context.params.meetupId;
+
+  print(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "A First Meetup",
+        image:
+          "https://alumni.ubc.ca/wp/wp-content/uploads/MyTownMeetups-1270x460-1.jpg",
+        address: "Some Address 5, 12345 Some City",
+        description: "",
+      },
+    },
+  };
 };
 
 export default MeetupDetails;
