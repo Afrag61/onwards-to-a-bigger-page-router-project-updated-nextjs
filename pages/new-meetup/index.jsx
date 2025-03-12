530cdca724a0b27f807f301f58d3e5496a5ddaeb
Ahmed Frag <a.frag166@gmail.com>
2025-03-12 13:42:29 +0200

    Introducing API Routes, Working with MongoDB, Sending Http Requests To Our API Routes

--- pages/new-meetup/index.jsx
+++ pages/new-meetup/index.jsx
@@ -1,10 +1,25 @@
-import Layout from "@/components/layout/Layout";
+import { useRouter } from "next/router";
+
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 import { print } from "@/Print";
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    print(enteredMeetupData);
+  const { push } = useRouter();
+
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(enteredMeetupData),
+    });
+
+    const data = await response.json();
+
+    print(data);
+
+    push("/");
   };
 
   return (
