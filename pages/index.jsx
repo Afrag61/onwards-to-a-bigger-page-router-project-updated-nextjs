1a616f0169225721c2de54b3ba844778d4591de3
Ahmed Frag <a.frag166@gmail.com>
2025-03-12 14:13:09 +0200

    Getting Data From The Database

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 import MeetupList from "@/components/meetups/MeetupList";
 import { print } from "@/Print";
 
@@ -28,25 +30,46 @@ const HomePage = ({ meetups }) => {
   );
 };
 
-export const getServerSideProps = (context) => {
-  const req = context.req;
-  const res = context.res;
+export const getStaticProps = async () => {
   // fetch data from api
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://afrag166:012722537853799A@cluster1.ttvkv.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster1"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
 
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => {
+        const { title, address, image, _id } = meetup;
+        const id = _id.toString();
+        return {
+          title,
+          address,
+          image,
+          id,
+        };
+      }),
     },
+    revalidate: 1,
   };
 };
 
-// export const getStaticProps = async () => {
+// export const getServerSideProps = (context) => {
+//   const req = context.req;
+//   const res = context.res;
 //   // fetch data from api
+
 //   return {
 //     props: {
 //       meetups: DUMMY_MEETUPS,
 //     },
-//     revalidate: 1,
 //   };
 // };
 
